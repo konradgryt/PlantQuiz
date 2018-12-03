9f10801fb144decedc9d21df14229185f787ca61
konradgryt <konrad.gryt@gmail.com>
2018-12-03 11:56:28 +0100

    Testing intents, camera intent and gallery intent, changes in the UI. Now app can use camera/gallery to display a picture

--- app/src/main/java/com/plantquiz/plantquiz/MainActivity.kt
+++ app/src/main/java/com/plantquiz/plantquiz/MainActivity.kt
@@ -1,24 +1,82 @@
 package com.plantquiz.plantquiz
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var _cameraButton: Button? = null
+    private var _photoGalleryButton: Button? = null
+    private var _imageTaken: ImageView? = null
+
+    val OPEN_CAMERA_BUTTON_REQUEST_ID = 1000
+    val OPEN_GALLERY_BUTTON_REQUEST_ID = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+    /*
         Toast.makeText(this, "The onCreate method is called ", Toast.LENGTH_SHORT).show()
         val myPlant: Plant = Plant("","","","","","",0)
        // Plant("test","test","test","","","",0)
         myPlant.plantName = "Some example name"
         var nameOfPlant = myPlant.plantName
+    */
+    /*
+        var flower = Plant()
+        var tree = Plant()
+
+        var collectionOfPlants: ArrayList<Plant> = ArrayList()
+        collectionOfPlants.add(flower)
+        collectionOfPlants.add(tree)
+    */
+
+        _cameraButton = findViewById(R.id.btnOpenCamera)
+        _photoGalleryButton = findViewById(R.id.btnOpenPhotoGallery)
+        _imageTaken = findViewById(R.id.imgTaken)
+
+        _cameraButton?.setOnClickListener{
+            Toast.makeText(this, "Camera button is clicked", Toast.LENGTH_SHORT).show()
+
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, OPEN_CAMERA_BUTTON_REQUEST_ID)
+        }
+
+        _photoGalleryButton?.setOnClickListener{
+            Toast.makeText(this, "Open gallery button is clicked", Toast.LENGTH_SHORT).show()
+
+            val galleryIntent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntent, OPEN_GALLERY_BUTTON_REQUEST_ID)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OPEN_CAMERA_BUTTON_REQUEST_ID) {
+            if (resultCode == Activity.RESULT_OK) {
+                val imageData = data?.extras?.get("data") as Bitmap
+                _imageTaken?.setImageBitmap(imageData)
+            }
+        }
+        if (requestCode == OPEN_GALLERY_BUTTON_REQUEST_ID) {
+            if (resultCode == Activity.RESULT_OK) {
+                val contentURI = data?.data
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                _imageTaken?.setImageBitmap(bitmap)
+            }
+        }
     }
 
     fun button1IsClicked(buttonView: View)  {
--- app/src/main/java/com/plantquiz/plantquiz/Plant.kt
+++ app/src/main/java/com/plantquiz/plantquiz/Plant.kt
@@ -3,6 +3,8 @@ package com.plantquiz.plantquiz
 class Plant(genus: String, species: String, cultivar: String, common: String,
             pictureName: String, description: String, difficulty: Int, id: Int = 0) {
 
+    constructor(): this("","","","","","",0)
+
     private var _plantName: String? = null
 
     var plantName: String?
